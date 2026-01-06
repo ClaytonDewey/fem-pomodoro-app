20b6fef82c7a5084c298170c30738e35ac166204
ClaytonDewey <clay@dryadmedia.com>
2026-01-06 16:19:10 -0600

    test: Replace time text

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,14 +1,14 @@
 import { CircularProgressbar } from 'react-circular-progressbar';
 
 const ProgressBar = () => {
-  const percentage = 66; // Example percentage value
+  const percentage = 82; // Example percentage value
   return (
     <div className='progress-wrapper'>
       <div className='progress-container-outer'>
         <div className='progress-container-inner'>
           <CircularProgressbar
             value={percentage}
-            text={`${percentage}%`}
+            text='17:59'
             strokeWidth={3}
           />
         </div>
